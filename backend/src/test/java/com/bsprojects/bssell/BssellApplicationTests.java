eccc536f5a774e233e5bc620cb3c118f25b044ad
Bruno Soares <brunosoaresbsv@gmail.com>
2021-05-05 21:37:43 -0300

    Project created

---
+++ backend/src/test/java/com/bsprojects/bssell/BssellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsprojects.bssell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BssellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
