d523866fbd6b63ce800a4b53ac320ae614ade251
BrunoSoaresGH <brunosoaresbsv@gmail.com>
2021-05-17 21:04:55 -0300

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -4,11 +4,11 @@ function DataTable() {
       <table className="table table-striped table-sm">
         <thead>
           <tr>
-            <th>Data</th>
-            <th>Vendedor</th>
-            <th>Clientes visitados</th>
-            <th>Negócios fechados</th>
-            <th>Valor</th>
+            <th>Date</th>
+            <th>Seller</th>
+            <th>Clients visited</th>
+            <th>Closed deals</th>
+            <th>Value</th>
           </tr>
         </thead>
         <tbody>
